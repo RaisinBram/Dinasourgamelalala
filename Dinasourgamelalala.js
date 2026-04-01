6c8329735c6cec1dea50b0706e3044bbcf0364c4
RaisinBram <rae.valdez@columbiachristian.com>
2026-04-01 09:06:40 -0700

    commit

---
+++ Dinasourgamelalala.js
@@ -0,0 +1,11 @@
+export default class Game {
+    constructor() {
+        const canvas = document.getElementById("myCanvas")
+        this.ctx = canvas.getContext("2d")
+    }
+
+    run() {
+        console.log("running the game")
+        //this.frame()
+    }
+}
