e4d1128cbb0d8493e15a2310d1f5b48d5b12d811
RaisinBram <rae.valdez@columbiachristian.com>
2026-04-15 09:12:20 -0700

    akngkgumv

--- Dino.js
+++ Dino.js
@@ -1,21 +1,10 @@
 export class Dino {
-    constructor() {
+    constructor(game) {
         //Create the positional and movement variables
         this.x = 100
         this.y = 100
         this.dy = 0
-
-        this.sprite_sheet = new Image()
-        this.sprite_sheet.src = 'dino_sprite.png'
-
-        this.sprites = {
-            "standing": { x: 677, y: 2, w: 44, h: 47 },
-            "walking1": { x: 765, y: 2, w: 44, h: 47 },
-            "walking2": { x: 809, y: 2, w: 44, h: 47 },
-            "bird flap down": { x: 1048, y: 2, w: 241, h: -45 },
-            "bird flap up": { x: 1241, y: 2, w: 195, h: 5},
-        }
-
+        this.game = game
         document.addEventListener("keydown", this.keydown.bind(this))
     }
 //bird flap down coor. = 239,2 top coor 
