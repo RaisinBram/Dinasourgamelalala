d08584de04e34fb83b0824182ad43a8b9f84f498
RaisinBram <rae.valdez@columbiachristian.com>
2026-04-20 08:41:46 -0700

    I'm very tired

--- Cacti.js
+++ Cacti.js
@@ -1,27 +1,13 @@
+import {sprite} from './sprite.js'
+
 export class Cacti {
     constructor (game) {
+        super(game)
          this.x = 600         //right side of 
          this.y = 200         //floor level
-         this.game = game
+
+         this.current_sprite = "cacti1"
     } 
 
-    draw(ctx) {
-        this.scale = 1.5
-        //draw Cacti sprite
-
-        ctx.drawImage(this.sprite_sheet,
-            sprite.x,
-            sprite.y,
-            sprite.w,
-            sprite.h,
-            //destination corner - upper right
-            this.x - sprite.w * this.scale / 2,
-            this.y - sprite.h * this.scale,
-            //destination scale
-            sprite.w * this.scale,
-            sprite.h * this.scale
-        )
-
-}
 
 }
