d2adb2cfecb7d32eb45ef0444cc844e4a4578627
Milene Zanon <milene.zanon@columbiachristian.com>
2026-04-24 09:05:00 -0700

    settings stuff

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+    // Y height of the game floor
+    floor_y: 400, 
+    //How strong is graity?
+    gravity_dy: 0.025,
+    // Dinosaur's initial vertical jump speed
+    jump_dy: 2,
+    //How high above the floor is the bird?
+    bird_height: 60,
+    // How fast does the bird move right-to-left?
+    bird_speed: 0.75,
+    // How fast does the cactus move right-to-left?
+    cactus_speed: 0.75,
+}
+
+export default settings;
